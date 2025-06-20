37724b2d0a7f27ddbf854ed01839e42f2d905790
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-06-20 23:22:29 +0530

    Add files via upload

---
+++ Chanlenger/Subscribe.java
@@ -0,0 +1,12 @@
+package src.Chanlenger;
+
+public class Subscribe {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+        System.out.println("* * * * *");
+
+    }
+}
