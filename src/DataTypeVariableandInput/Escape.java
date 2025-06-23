70e5733076bd004de7b12cbe828955041b471886
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-06-23 11:02:16 +0530

    Add files via upload

---
+++ src/DataTypeVariableandInput/Escape.java
@@ -0,0 +1,8 @@
+package src.DataTypeVariableandInput;
+
+public class Escape {
+    public static void main(String[] args) {
+     //   System.out.println("Hello \"KGCoding\"....");   //    \" ---> Escape sequence
+        System.out.println("Hello \\KGCoding\\....");
+    }
+}
