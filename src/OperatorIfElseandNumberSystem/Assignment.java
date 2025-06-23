daaa13e3d59b2872718e47eb470946e81c953f46
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-06-23 11:04:43 +0530

    Add files via upload

---
+++ src/OperatorIfElseandNumberSystem/Assignment.java
@@ -0,0 +1,12 @@
+package src.OperatorIfElseandNumberSystem;
+
+public class Assignment {
+    public static void main(String[] args) {
+        int myInt =9;     // '=' ---> assignment operator
+        System.out.println(myInt);
+        int newInt= myInt;
+        System.out.println(newInt);
+
+        // 9=myInt; ---> is not defined because right ki value utha ke left me dalta hai
+    }
+}
