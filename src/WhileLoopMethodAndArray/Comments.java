dc572522e5742cabe312842afccb071e597cc8d5
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-06-25 20:34:19 +0530

    Add files via upload

---
+++ src/WhileLoopMethodAndArray/Comments.java
@@ -0,0 +1,17 @@
+package src.WhileLoopMethodAndArray;
+
+public class Comments {
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        //this is  important, don't delete this
+        System.out.println("Hello World");
+
+        /*
+        this is the first line of the comment
+        the comment is  still continuing
+         */
+    }
+}
