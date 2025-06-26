070285a485388da60e367cdbbb11b30dc84ff324
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-06-26 22:57:35 +0530

    Add files via upload

---
+++ src/WhileLoopMethodAndArray/Function.java
@@ -0,0 +1,15 @@
+package src.WhileLoopMethodAndArray;
+
+public class Function {
+    public static void main(String[] args) {
+        System.out.println("In Main method");
+        greetUser();
+        System.out.println("Method calling complete");
+
+    }
+
+        public static void greetUser(){
+            System.out.println("Good Morning from KGCoding");
+        }
+
+}
