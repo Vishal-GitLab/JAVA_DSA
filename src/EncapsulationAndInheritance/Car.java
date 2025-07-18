fb8a3228ba08100223e9bd323df3fc5e7fe74f8a
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-07-18 22:44:22 +0530

    Add files via upload

---
+++ src/EncapsulationAndInheritance/Car.java
@@ -0,0 +1,33 @@
+package src.EncapsulationAndInheritance;
+
+public class Car {
+
+    public String color;
+    public String model;
+    private double fuelLevel;
+    // private cheezo ko ,, jo jis class ke andar hai vahi use kar sakti hai , chahe vo methods bnaye ya constructor bnaye
+    private long costOfPurchase;
+
+
+    public Car() {                  // using the defaukt constructor
+
+    }
+
+    public Car(String color, String model, double fuelLevel, long costOfPurchase) {
+        this.color = color;
+        this.model = model;
+        this.fuelLevel = fuelLevel;
+        this.costOfPurchase = costOfPurchase;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Car{");
+        sb.append("color='").append(color).append('\'');
+        sb.append(", model='").append(model).append('\'');
+        sb.append(", fuelLevel=").append(fuelLevel);
+        sb.append(", costOfPurchase=").append(costOfPurchase);
+        sb.append('}');
+        return sb.toString();
+    }
+}
