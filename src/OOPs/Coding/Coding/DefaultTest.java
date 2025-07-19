f82e72de817319b910585368d7329cec61c34270
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-07-19 17:52:44 +0530

    Add files via upload

---
+++ src/OOPs/Coding/Coding/DefaultTest.java
@@ -0,0 +1,10 @@
+package src.Coding;
+
+import src.OOPs.Car;
+
+public class DefaultTest {
+    public static void main(String[] args) {
+        Car car = new Car("Yellow","Dezire",
+                5,3000);
+    }
+}
