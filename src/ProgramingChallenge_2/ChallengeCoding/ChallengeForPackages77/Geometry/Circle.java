044d9378b5d26956e22ce941455b566c5bf6a216
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-07-19 18:02:38 +0530

    Add files via upload

---
+++ src/ProgramingChallenge_2/ChallengeCoding/ChallengeForPackages77/Geometry/Circle.java
@@ -0,0 +1,10 @@
+package src.ProgramingChallenge_2.ChallengeForPackages77.Geometry;
+
+public class Circle {
+    public double radius;
+
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+}
