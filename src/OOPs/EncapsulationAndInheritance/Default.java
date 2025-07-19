de78531194f7884e7dc6db469c810a3b213fb391
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-07-19 17:54:25 +0530

    Add files via upload

---
+++ src/OOPs/EncapsulationAndInheritance/Default.java
@@ -0,0 +1,11 @@
+package src.OOPs;
+
+ class Default {
+
+
+
+  // public -->. koi bhi access karega
+  //  default -->. mere package ke andar
+   // private -->. sif mere class ke nadar hi use hoga bas
+
+}
