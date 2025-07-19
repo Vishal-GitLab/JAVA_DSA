6536819a5e59b42616a63932b7b0785709e84b82
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-07-19 17:51:54 +0530

    Add files via upload

---
+++ src/OOPs/EncapsulationAndInheritance/Packages.java
@@ -0,0 +1,10 @@
+package src.OOPs;
+
+import java.util.Scanner;
+
+public class Packages {
+    public static void main(String[] args) {
+        Scanner input =  new Scanner(System.in);
+    }
+}
+
