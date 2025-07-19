00e8eb2fd2994c9e1a8aa8d6d2e4e2f8f3e80a96
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-07-19 18:01:57 +0530

    Add files via upload

---
+++ src/ProgramingChallenge_2/ChallengeCoding/ChallengeForPackages77/Geometry/Rectangle.java
@@ -0,0 +1,11 @@
+package src.ProgramingChallenge_2.ChallengeForPackages77.Geometry;
+
+public class Rectangle {
+    public  int length;
+    public int breadth;
+
+    public Rectangle(int length, int breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+}
