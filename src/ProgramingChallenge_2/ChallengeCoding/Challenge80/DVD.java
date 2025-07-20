9238e9cfc9eabe9d5fb8122fa653c9faeb056cc6
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-07-20 22:41:40 +0530

    Add files via upload

---
+++ src/ProgramingChallenge_2/ChallengeCoding/Challenge80/DVD.java
@@ -0,0 +1,8 @@
+package src.ProgramingChallenge_2.ChallengeCoding.Challenge80;
+
+public class DVD extends LibraryItem {                     // ab ye child class ban gayi hai
+
+
+     private int durationInSeconds;
+
+}
