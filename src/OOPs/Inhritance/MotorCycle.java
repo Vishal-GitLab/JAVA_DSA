379b4a0b50eb8dfbfc06008d02c74642aa28e8b6
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-07-20 17:45:05 +0530

    Add files via upload

---
+++ src/OOPs/Inhritance/MotorCycle.java
@@ -0,0 +1,10 @@
+package src.OOPs.Inhritance;
+
+public class MotorCycle extends TwoWheeler {
+    public double petrolCapacity;
+
+
+    public void start() {
+        System.out.println("Starting");
+    }
+}
