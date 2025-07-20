20874fde73944baa9dfd32a1980bdac0ffc7b60a
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-07-20 22:43:30 +0530

    Add files via upload

---
+++ src/ProgramingChallenge_2/ChallengeCoding/Challenge82/TestArray.java
@@ -0,0 +1,10 @@
+package src.ProgramingChallenge_2.ChallengeCoding.Challenge82;
+
+public class TestArray {
+    public static void main(String[] args) {
+        ArrayOperations opr = new ArrayOperations(new int[] {1,3,5,9});
+        ArrayOperations.Statistics statistics =  opr.new Statistics();
+        System.out.println(statistics.mean());
+        
+    }
+}
