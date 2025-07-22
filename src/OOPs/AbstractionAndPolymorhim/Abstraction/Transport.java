764f28461c43631154bd6ee6ff549e9e7d3a33ef
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-07-22 16:42:19 +0530

    Add files via upload

---
+++ src/OOPs/AbstractionAndPolymorhim/Abstraction/Transport.java
@@ -0,0 +1,7 @@
+package src.OOPs.AbstractionAndPolymorhim.Abstraction;
+
+public interface Transport {                    // transport ham class nahi interface bnaye hai
+                      // iske anadr public abstract chize hi difine kar sakte hai
+     void getSetGo();           //   yha hm jo bhi method define karenge public abstract type ke hone
+
+}
