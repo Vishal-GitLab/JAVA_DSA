fcb73dadb7b9c777cc63b414831763f636f0581c
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-07-23 16:41:09 +0530

    Add files via upload

---
+++ src/ProgramingChallenge_3/Challenge84/Eagle.java
@@ -0,0 +1,11 @@
+package src.ProgramingChallenge_3.Challenge84;
+
+public class Eagle extends Bird{
+    public Eagle() {
+        super("Eagle");
+    }
+    @Override
+    public void fly() {
+        System.out.println("Eagle is flying");
+    }
+}
