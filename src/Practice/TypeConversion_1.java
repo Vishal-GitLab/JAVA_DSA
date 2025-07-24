22dc8031bad212fd2ea9cffbd4f78beee19db657
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-07-24 22:37:31 +0530

    Add files via upload

---
+++ src/Practice/TypeConversion_1.java
@@ -0,0 +1,11 @@
+package src.Practice;
+
+public class TypeConversion_1 {
+    public static void main(String[] args) {
+        float myFloat = 5f;
+        System.out.println(myFloat);
+        int myInt = (int)5.0f;
+        System.out.println(myInt);
+
+    }
+}
