532d83f0d7bf069f69fb688ac73bf372c0457d07
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-07-25 19:29:07 +0530

    Add files via upload

---
+++ src/Practice/Coding/Assighnment_6.java
@@ -0,0 +1,10 @@
+package src.Practice;
+
+public class Assighnment_6 {
+    public static void main(String[] args) {
+        int myInt = 5;
+        System.out.println(myInt);
+        int yourInt = myInt;
+        System.out.println(yourInt);
+    }
+}
