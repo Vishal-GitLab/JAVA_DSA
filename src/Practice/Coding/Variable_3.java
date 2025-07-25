81dd1738857b969cf18343d874c87b36d245d15d
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-07-25 19:27:05 +0530

    Add files via upload

---
+++ src/Practice/Coding/Variable_3.java
@@ -0,0 +1,18 @@
+package src.Practice;
+
+public class Variable_3 {
+    public static void main(String[] args) {
+
+        int myNumber = 786;
+        int yourNumber;
+        yourNumber =787;
+        System.out.println(yourNumber);
+
+        float number  = 5;
+        System.out.println(number);
+
+        double myFloat = 8.98;
+        System.out.println(myFloat);
+
+    }
+}
