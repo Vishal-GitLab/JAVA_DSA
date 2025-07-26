260355de75feed0ae992336cb4460e2ee45c8819
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-07-26 17:15:27 +0530

    Add files via upload

---
+++ src/OOPs/AbstractionAndPolymorhim/Polymorhism/Plane.java
@@ -0,0 +1,8 @@
+package src.OOPs.AbstractionAndPolymorhism.Polymorhism;
+
+ class Plane implements Vehicle {
+     @Override
+     public void start() {
+         System.out.println("Plane is taking off ");
+     }
+ }
