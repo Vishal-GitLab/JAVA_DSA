7a5f9361a2fa97769597c9baccca2415767a7d1b
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-07-27 17:29:38 +0530

    Add files via upload

---
+++ src/ProgramingChallenge_3/Challenge85/Calculator.java
@@ -0,0 +1,24 @@
+package src.ProgramingChallenge_3.Challenge85;
+
+import src.OOPs.GetterSetter.Car;
+
+public class Calculator {
+    public int add(int first, int second) {
+        return first + second;
+    }
+    public int add(int first, int second, int third) {
+        return first + second + third;
+    }
+    public double add(double first, double second) {
+        return  first + second;
+    }
+
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println(calc.add(5,6));
+        System.out.println(calc.add(5,6,7));
+        System.out.println(calc.add(5.9,6.2));
+    }
+
+}
