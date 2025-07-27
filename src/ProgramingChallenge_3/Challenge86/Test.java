f38d5b7cc8f1a247d3978465c3e8c93b78c4c307
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-07-27 17:30:50 +0530

    Add files via upload

---
+++ src/ProgramingChallenge_3/Challenge86/Test.java
@@ -0,0 +1,10 @@
+package src.ProgramingChallenge_3.Challenge86;
+
+public class Test {
+    public static void main(String[] args) {
+        Car car = new Car();
+        Vehicle veh = new Vehicle();
+        veh.service();
+        car.service();
+    }
+}
