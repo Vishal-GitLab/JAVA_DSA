905ad5a9e70231ab88500fbf2863b5bf2bbce9b8
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-08-05 16:36:16 +0530

    Add files via upload

---
+++ src/Practice/Coding/Arithmetic.java
@@ -0,0 +1,21 @@
+package src.Practice.Coding;
+
+public class Arithmetic {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a+b);
+        System.out.println(a%b);
+
+      double x = 5;
+      double y = 8;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+
+    }
+}
