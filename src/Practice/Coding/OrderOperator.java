8f173b3c7c92f05a838449fd4bf3b8aea654b881
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-08-05 16:38:54 +0530

    Add files via upload

---
+++ src/Practice/Coding/OrderOperator.java
@@ -0,0 +1,10 @@
+package src.Practice.Coding;
+
+public class OrderOperator {
+    public static void main(String[] args) {
+        System.out.println(9*3-2);
+
+        System.out.println(9/3/3);
+        System.out.println(9/(3*3)-5);
+    }
+}
