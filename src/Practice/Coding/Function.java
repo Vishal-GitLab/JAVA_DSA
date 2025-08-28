2359fc0f148060d233849f803f404fb7828d7129
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-08-28 23:03:22 +0530

    Add files via upload

---
+++ src/Practice/Coding/Function.java
@@ -0,0 +1,14 @@
+package src.Practice.Coding;
+
+import java.sql.SQLOutput;
+
+public class Function {
+    public static void main(String[] args) {
+        System.out.println("In main method");
+        greetUser();
+        System.out.println("Calling complete");
+    }
+    public  static void greetUser() {
+        System.out.println("Good morning");
+    }
+}
