4be8cb6f5df312e3925bfb41245d586acb2f82aa
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-08-28 22:54:53 +0530

    Add files via upload

---
+++ src/Practice/Coding/Loop.java
@@ -0,0 +1,17 @@
+package src.Practice.Coding;
+
+public class Loop {
+    public static void main(String[] args) {
+        int num = 1;
+        while (num <= 10) {
+            System.out.println(num);
+            num++;
+        }
+        System.out.println();
+        int count = 500;
+        while(count >= 200 ) {
+            System.out.println(count);
+            count--;
+        }
+    }
+}
