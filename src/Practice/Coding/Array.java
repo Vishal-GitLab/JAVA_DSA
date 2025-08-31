44c03bd190f1702c4137695f5ecd461fc3c51bce
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-08-31 19:36:40 +0530

    Add files via upload

---
+++ src/Practice/Coding/Array.java
@@ -0,0 +1,15 @@
+package src.Practice.Coding;
+
+public class Array {
+    public static void main(String[] args) {
+
+
+        int[] myArr = {10, 56,76,98,54};
+
+        int index = 0;
+        while (index < 5) {
+            System.out.println(myArr[index]);
+            index++;
+        }
+    }
+}
