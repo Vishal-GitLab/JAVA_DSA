513ce9c65cb41f3441db3e9f854db26329a1b226
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-09-11 11:21:00 +0530

    Add files via upload

---
+++ src/Practice/Challenge/Challenge_50/ArrayUtility.java
@@ -0,0 +1,51 @@
+package src.Practice.Challenge.Challenge_50;
+
+import java.util.Scanner;
+
+public class ArrayUtility {
+    public static int[] inputArray() {              // array se intezer input krane ke liye
+        Scanner input = new Scanner(System.in);
+//        System.out.println("Welcome to Array sum Average  ");
+        System.out.print("Please enter the number of elements: ");
+        int size = input.nextInt();
+        int[] nums = new int[size];          // jo size array ne  diya  tha usi size ka array declare kiya jata hai
+        int i = 0;
+        while (i < size ) {
+            System.out.print("Please enter element no: " + (i+1) + ": ");
+            nums[i] = input.nextInt();    // number ko array ke andar dalne ke liye i^th position par
+            i++;
+        }
+        return nums;
+    }
+    public static int[][] input2DArray() {
+        Scanner input = new Scanner(System.in);
+//        System.out.println("Welcome to Array sum Average  ");
+        System.out.print("Please enter the number of rows: ");
+        int rows = input.nextInt();
+        System.out.print("Please enter the number of columns:");
+        int columns = input.nextInt();
+        int[][] numArray = new int[rows][columns];
+
+
+
+        int i = 0;
+        while (i < rows ) {
+            int j = 0;
+            while (j < columns) {
+                System.out.print("Please enter element row: " + (i + 1)
+                        + ", column: " + (j + 1) + " :");
+                numArray[i][j] = input.nextInt();
+                j++;
+            }
+            i++;
+        }
+        return numArray;
+    }
+    public static void displayArray(int[] numArray) {
+        int i = 0;
+        while (i < numArray.length) {
+            System.out.print(numArray[i] + " ");
+            i++;
+        }
+    }
+}
