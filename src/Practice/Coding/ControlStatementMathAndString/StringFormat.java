5910ff7ff76fa004d89686718ca2a1aed8a90726
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-09-16 21:57:50 +0530

    Add files via upload

---
+++ src/Practice/Coding/ControlStatementMathAndString/StringFormat.java
@@ -0,0 +1,10 @@
+package src.Practice.Coding.ControlStatementMathAndString;
+
+public class StringFormat {
+    public static void main(String[] args) {
+        String name = "Vishal";
+        int marks = 45;
+        System.out.println("Hello " + name + ", Your marks are: " + marks );
+        System.out.printf("Hello %s, your marks are:  %d", name, marks);
+    }
+}
