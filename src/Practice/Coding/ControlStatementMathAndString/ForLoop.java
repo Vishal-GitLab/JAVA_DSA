056d66ba8bd77bd3c94cc30b99ae557885f77579
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-09-16 21:57:18 +0530

    Add files via upload

---
+++ src/Practice/Coding/ControlStatementMathAndString/ForLoop.java
@@ -0,0 +1,10 @@
+package src.Practice.Coding.ControlStatementMathAndString;
+
+public class ForLoop {
+    public static void main(StringFormat[] args) {
+        int i;
+        for (i = 1; i <= 100; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
