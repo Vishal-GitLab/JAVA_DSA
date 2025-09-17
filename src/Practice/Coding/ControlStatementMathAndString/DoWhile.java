2b7c148e7935dcb8296358475c8c615451bd84ca
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-09-17 11:19:33 +0530

    Add files via upload

---
+++ src/Practice/Coding/ControlStatementMathAndString/DoWhile.java
@@ -0,0 +1,17 @@
+package src.Practice.Coding.ControlStatementMathAndString;
+
+import java.util.Scanner;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int age;
+        do {
+            System.out.print("Enter your age: ");
+            age = input.nextInt();
+        } while ( age < 0 || age > 100);
+            System.out.println("Your age is: " + age);
+        }
+    }
+
