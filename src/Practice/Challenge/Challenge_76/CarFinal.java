1d612da9f565ff81ed0a329fcd8f32b4c17aef39
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-09-23 17:55:41 +0530

    Add files via upload

---
+++ src/Practice/Challenge/Challenge_76/CarFinal.java
@@ -0,0 +1,16 @@
+package src.Practice.Challenge.Challenge_76;
+
+import src.ProgramingChallenge_3.Challenge85.Calculator;
+
+public class CarFinal {
+    final  int noOfWheels;
+    final String model;
+    final String engineInLiters;
+
+
+    public CarFinal(String model, String engineInLiters, int noOfWheels) {
+        this.noOfWheels = noOfWheels;
+        this.model = model;
+        this.engineInLiters = engineInLiters;
+    }
+}
