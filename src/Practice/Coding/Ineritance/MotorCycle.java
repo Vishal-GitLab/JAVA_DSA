eb742ec091a0225fdabe62d2d3a869ef43f11e2e
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-09-24 23:58:43 +0530

    Add files via upload

---
+++ src/Practice/Coding/Ineritance/MotorCycle.java
@@ -0,0 +1,9 @@
+package src.Practice.Coding.Ineritance;
+
+public class MotorCycle extends TwoWheeler {
+    public double petrolCapacity;
+
+    public void Start() {
+        System.out.println("Starting");
+    }
+}
