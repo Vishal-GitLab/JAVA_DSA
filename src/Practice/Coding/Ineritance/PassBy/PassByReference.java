e6843a603c1901113a893f4a73ced33f0fd1d8ac
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-09-26 23:09:42 +0530

    Add files via upload

---
+++ src/Practice/Coding/Ineritance/PassBy/PassByReference.java
@@ -0,0 +1,21 @@
+package src.Practice.Coding.Ineritance.PassBy;
+
+public class PassByReference {
+    public static void main(String[] args) {
+
+    }
+
+
+    public static void Move(Point p)
+
+    public static class Point {
+        int x;
+        int y;
+
+        public Point(int x, int y ) {
+            this.x = x;
+            this.y = y;
+        }
+
+    }
+}
