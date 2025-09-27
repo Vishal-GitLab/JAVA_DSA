a3e05b8fb7520f7466af716a8fa70a514fb32778
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-09-27 12:29:59 +0530

    Add files via upload

---
+++ src/Practice/Challenge/Challenge_80/LibraryItem.java
@@ -0,0 +1,16 @@
+package src.Practice.Challenge.Challenge_80;
+
+public class LibraryItem {
+
+    private String itemId;
+    private String title;
+    private String author;
+
+    public void checkout() {
+        System.out.println("checkout");
+    }
+
+    public void returnItem() {
+        System.out.println("Returning Number");
+    }
+}
