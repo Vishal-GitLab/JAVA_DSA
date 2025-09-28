5e12c652536ee2666df38e4beeeababef01e1074
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-09-28 15:23:29 +0530

    Add files via upload

---
+++ src/Practice/Coding/Abstract/TestAbstraction.java
@@ -0,0 +1,9 @@
+package src.Practice.Coding.Abstract;
+
+public class TestAbstraction {
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.Commute();
+        car.getSetGo();
+    }
+}
