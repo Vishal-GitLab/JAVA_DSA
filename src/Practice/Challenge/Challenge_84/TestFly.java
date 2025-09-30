e7f16625a0eb570f430a3c285fbcf32ef00a1e4b
St. Vishal Chusasiya <vishal_246045@saitm.ac.in>
2025-09-30 22:31:30 +0530

    Add files via upload

---
+++ src/Practice/Challenge/Challenge_84/TestFly.java
@@ -0,0 +1,8 @@
+package src.Practice.Challenge.Challenge_84;
+
+public class TestFly {
+    public static void main(String[] args) {
+        Eagle eagle =  new Eagle();
+        eagle.Fly();
+    }
+}
