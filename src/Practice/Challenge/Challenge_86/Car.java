c8467247425644b03a0b2b68e58ecb671aa49e44
St. Vishal Chusasiya <vishal_246045@saitm.ac.in>
2025-10-04 23:28:52 +0530

    Add files via upload

---
+++ src/Practice/Challenge/Challenge_86/Car.java
@@ -0,0 +1,9 @@
+package src.Practice.Challenge.Challenge_86;
+
+public class Car extends Vehicle {
+    @Override
+    public void service() {
+        super.service();
+        System.out.println("Your car is getting start");
+    }
+}
