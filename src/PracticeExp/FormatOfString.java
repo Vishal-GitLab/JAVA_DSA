24a106b00b608ac783459434b129c21e7cb9f946
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-12-22 21:47:46 +0530

    Add files via upload

---
+++ src/PracticeExp/FormatOfString.java
@@ -0,0 +1,11 @@
+package src.PracticeExp;
+
+public class FormatOfString {
+    public static void main(String[] args) {
+        String name =  "Chaursaiya";
+        int  marks =  345;
+
+        System.out.println("Hello " + name + " , Your marks is: " + marks);
+        System.out.printf(" Hello %s,  Your marks is: %d " , name , marks);
+    }
+}
