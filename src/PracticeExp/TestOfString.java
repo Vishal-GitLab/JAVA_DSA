db035be1f7af24657f0f228efff6276076457320
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-12-23 21:56:30 +0530

    Add files via upload

---
+++ src/PracticeExp/TestOfString.java
@@ -0,0 +1,11 @@
+package src.PracticeExp;
+
+public class TestOfString {
+    public static void main(String[] args) {
+        StringBuilder sb =  new StringBuilder("First ");
+        sb.append(45.3);
+        sb.append(" Your marka is :");
+        sb.append(432);
+        System.out.println(sb);
+    }
+}
