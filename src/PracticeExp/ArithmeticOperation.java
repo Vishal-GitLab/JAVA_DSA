2ef0e7ff2bcee01be2adde39f02143df93ec5804
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-12-27 21:32:38 +0530

    Add files via upload

---
+++ src/PracticeExp/ArithmeticOperation.java
@@ -0,0 +1,20 @@
+package src.PracticeExp;
+
+public class ArithmeticOperation {
+    public static void main(String[] args) {
+        int x = 45;
+        int y = 6;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+        System.out.println(x%y);
+
+        double p = 23;
+        double q = 67;
+        System.out.println(p+q);
+        System.out.println(p-q);
+        System.out.println(p*q);
+        System.out.println(p/q);
+    }
+}
