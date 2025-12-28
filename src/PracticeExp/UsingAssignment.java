2323b34c9a65fb450097c7a61a942e98111aef31
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-12-28 22:28:58 +0530

    Add files via upload

---
+++ src/PracticeExp/UsingAssignment.java
@@ -0,0 +1,11 @@
+package src.PracticeExp;
+
+public class UsingAssignment {
+    public static void main(String[] args) {
+        int myInt = 10;
+
+        System.out.println(myInt);
+        int newInt= myInt;
+        System.out.println(newInt);
+    }
+}
