3552f81185cc0aa8aad49fe9961d3bbcd8b24e4e
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-12-29 20:49:57 +0530

    Add files via upload

---
+++ src/PracticeExp/IfElse.java
@@ -0,0 +1,18 @@
+package src.PracticeExp;
+
+public class IfElse {
+    public static void main(String[] args) {
+        boolean isFemale = true;
+        String name ="Pushpa";
+
+
+
+        if (isFemale) {
+            System.out.println("Ms. " + name);
+        }
+        else {
+            System.out.println("Mr." + name);
+        }
+
+    }
+}
