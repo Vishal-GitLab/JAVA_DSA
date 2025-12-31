125f0cad944d08e76a7b7426899daa5532a1ab9e
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-12-31 21:50:30 +0530

    Add files via upload

---
+++ src/PracticeExp/UsingOrderOperator.java
@@ -0,0 +1,9 @@
+package src.PracticeExp;
+
+public class UsingOrderOperator {
+    public static void main(String[] args) {
+        System.out.println(6-7);
+
+        System.out.println(5-4/6);
+    }
+}
