19911a7d6473e6326d7176c7581cc7bd7c204ec9
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2026-01-01 20:31:46 +0530

    Add files via upload

---
+++ src/PracticeExp/UsingRelationalOperator.java
@@ -0,0 +1,16 @@
+package src.PracticeExp;
+
+import java.util.Scanner;
+
+public class UsingRelationalOperator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        int age = input.nextInt();
+        if (age >= 18) {
+            System.out.println("You are eligible to driving ");
+        } else {
+            System.out.println("Beta abhi tumhari abhi age nhi huii h ");
+        }
+    }
+}
