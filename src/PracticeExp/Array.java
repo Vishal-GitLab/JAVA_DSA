9ea10f226a8f39cdd10ebfc8a9bf5e8af8f1b640
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2026-01-08 21:16:36 +0530

    Add files via upload

---
+++ src/PracticeExp/Array.java
@@ -0,0 +1,12 @@
+package src.PracticeExp;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] arr = {232,45,76,89,34};
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+}
