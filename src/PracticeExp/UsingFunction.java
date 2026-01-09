8bf78d57e250de258b95356d4cec9dc482b3d3ce
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2026-01-09 14:20:54 +0530

    Add files via upload

---
+++ src/PracticeExp/UsingFunction.java
@@ -0,0 +1,14 @@
+package src.PracticeExp;
+
+public class UsingFunction {
+    public static void main(String[] args) {
+        System.out.println("In main method calling ");
+        greetUser();
+
+    }
+
+    public static void greetUser(){
+        System.out.println("Good Morning from KGCoding");
+    }
+    }
+
