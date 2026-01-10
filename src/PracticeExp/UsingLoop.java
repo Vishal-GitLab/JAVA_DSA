9ff32b44d5bbeca0689aebb3bdafddc6cd7f9578
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2026-01-10 23:33:59 +0530

    Add files via upload

---
+++ src/PracticeExp/UsingLoop.java
@@ -0,0 +1,11 @@
+package src.PracticeExp;
+
+public class UsingLoop {
+    public static void main(String[] args) {
+        int count = 500;
+        while (count >= 200) {
+            System.out.println(count);
+            count -= 1;
+        }
+    }
+}
